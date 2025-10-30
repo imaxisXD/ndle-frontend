ef1a7d5d2e32b59bce08f86f467e8dac1f3092b9
imaxisXD <sunny735084@gmail.com>
2025-10-30 15:52:28 +0530

    Add API URL configuration for OpenPanel integration in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,7 @@ export default function RootLayout({
                 (window.op.q = window.op.q || []).push(args);
               };
               window.op('init', {
+                apiUrl: 'https://openpanel-api-production-b833.up.railway.app/api', 
                 clientId: '3fee2715-1da7-4c02-a23f-e9fa96094c1b',
                 trackScreenViews: true,
                 trackOutgoingLinks: true,
