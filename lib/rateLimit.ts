d15d2ba4e1ab1dab80a137be37d1eeda6a326b8a
imaxisXD <sunny735084@gmail.com>
2026-01-01 02:50:48 +0530

    fix: use server

--- lib/rateLimit.ts
+++ lib/rateLimit.ts
@@ -1,15 +1,19 @@
-"use server";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
-});
+const redis =
+  process.env.UPSTASH_REDIS_REST_URL && process.env.UPSTASH_REDIS_REST_TOKEN
+    ? new Redis({
+        url: process.env.UPSTASH_REDIS_REST_URL,
+        token: process.env.UPSTASH_REDIS_REST_TOKEN,
+      })
+    : null;
 
-export const rateLimit = new Ratelimit({
-  redis,
-  limiter: Ratelimit.slidingWindow(10, "10 s"),
-  prefix: "@upstash/ratelimit",
-  analytics: true,
-});
+export const rateLimit = redis
+  ? new Ratelimit({
+      redis,
+      limiter: Ratelimit.slidingWindow(10, "10 s"),
+      prefix: "@upstash/ratelimit",
+      analytics: true,
+    })
+  : null;
